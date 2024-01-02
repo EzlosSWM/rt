ecf038a6668630af6b85347bce17e7560cd55af9
EzlosSWM <ezlosswm@gmail.com>
2024-01-02 13:12:53 -0600

    page:about

---
+++ src/data/contactInfo.ts
@@ -0,0 +1,12 @@
+const contactInfo = [
+  {
+    icon: "mdi:email",
+    detail: "info@mysite.com",
+  },
+  {
+    icon: "mdi:phone",
+    detail: "123-456-7890",
+  },
+];
+
+export default contactInfo;
