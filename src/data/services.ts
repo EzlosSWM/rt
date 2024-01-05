2442e73908bb564330119c75bd63b9423ec65d78
EzlosSWM <ezlosswm@gmail.com>
2024-01-04 22:49:48 -0600

    page:services

---
+++ src/data/services.ts
@@ -0,0 +1,28 @@
+const services = [
+  {
+    icon: "mdi:church",
+    title: "Wedding",
+  },
+  {
+    icon: "mdi:business",
+    title: "Commercial",
+  },
+  {
+    icon: "mdi:soccer",
+    title: "Sports",
+  },
+  {
+    icon: "mdi:photography",
+    title: "Portraits",
+  },
+  {
+    icon: "mdi:edit",
+    title: "Photo editing",
+  },
+  {
+    icon: "mdi:soccer",
+    title: "Misc",
+  },
+];
+
+export default services;
